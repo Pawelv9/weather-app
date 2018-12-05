00779821c428f6bcde4b95f06573a457a16f814c
Pavel <Pawelv9@gmail.com>
2018-12-05 18:45:32 +0300

    adding geolocation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Application = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        err => console.log(err)
+    );
+  
+    return (
+    <div>
+        Start here     
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Application />, document.querySelector('#root'))
