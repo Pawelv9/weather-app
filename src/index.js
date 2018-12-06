e40ee4d1e30fc47848289193c88793932a624255
Pavel <Pawelv9@gmail.com>
2018-12-06 10:19:15 +0300

    adding state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 // import SeasonDisplay from './SeasonDisplay';
 
-const Application = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        err => console.log(err)
-    );
-  
-    return (
-    <div>
-        Start here     
-    </div>
-  )
+class Application extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => { this.setState({
+                            lat:
+                              position.coords.latitude
+                          });
+                        },
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
+            );
+    }
+    
+
+    render () {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Lat: {this.state.lat}</div>
+        }
+
+        return <div>Loading emojis..</div>
+    }
 }
 
 ReactDOM.render(<Application />, document.querySelector('#root'))
